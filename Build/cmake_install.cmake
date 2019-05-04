11a2c90d386aad1201eb7eab894f6b8b294f38be
aflathorea <aflathorea@gmail.com>
2019-05-04 20:59:43 +0300

    Add files via upload

--- Build/cmake_install.cmake
+++ Build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/horea/Documents/qt_projects/Hazard02
+# Install script for directory: /home/horea/Downloads/Hazard-master/Source
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/horea/Documents/qt_projects/build-Hazard02-Desktop-Default/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/horea/Downloads/Hazard-master/Build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
